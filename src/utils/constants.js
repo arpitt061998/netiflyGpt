aab32b6c38771eaecc9c333ede8f8e4b6f92a739
Arpit <cu.17bcs2902@gmail.com>
2023-10-12 00:53:21 +0530

    changing useravatar

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 
 export const LOGO_URL = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
-export const USER_AVATAR =  "https://media.licdn.com/dms/image/C4D03AQGod45ATWcztQ/profile-displayphoto-shrink_400_400/0/1659365951587?e=1700092800&v=beta&t=uRCMGC4LG4_JGCshsTJTi0RWfvr1xiPkrBLhL2dVwuw";
+export const USER_AVATAR = "https://cdn.iconscout.com/icon/premium/png-256-thumb/profile-1965627-1657225.png?f=webp&w=256";
 
 export const TMDB_OPTIONS = {
   method: 'GET',
